d5cecf6c5a081e33207483ea7deb7f06a1a3fd52
Lum0ss <t.heitmann99@gmail.com>
2024-12-15 01:48:22 +0100

    ui changes

--- app/src/main/java/com/atom/lina/MainActivity.java
+++ app/src/main/java/com/atom/lina/MainActivity.java
@@ -34,4 +34,38 @@ public class MainActivity extends AppCompatActivity {
         TextView matrixTestView = findViewById(R.id.matrixTestView);
         matrixTestView.setText("Matrix: " + matrixA);
     }
+
+    private SimpleMatrix readMatrix(String input){
+        // split string into rows
+        String[] rows = input.split("\n");
+        int n = rows.length;
+        int m = rows[0].split(" ").length;
+        double[][] matrixVals = new double[n][m];
+        boolean sameLength = true;
+        for(int i = 0; i < n; i++){
+            String[] cols = rows[i].split(" ");
+            int m_ = cols.length;
+            if(m_ != m){
+                sameLength = false;
+                break;
+            }
+            for(int j = 0; j < m; j++) {
+                try {
+                    double value = Double.parseDouble(cols[j]);
+                    matrixVals[i][j] = value;
+                } catch (NumberFormatException e) {
+                    System.out.println("Cannot convert input: " + cols[j]);
+                }
+            }
+        }
+        // check if all rows are of the same length
+        if(!sameLength){
+            System.out.println("Invalid matrix dimensions");
+            SimpleMatrix errMat = SimpleMatrix.filled(n, m, 0.0);
+            return errMat;
+        }
+        // return the correct matrix
+        SimpleMatrix matrix = new SimpleMatrix(matrixVals);
+        return matrix;
+    }
 }
